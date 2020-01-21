79ca1c648cc70faf034aaf18d9dcdbd6b8454714
Prams <pgustiarum@gmail.com>
2020-01-21 22:26:41 +0700

    add dots, still error tho

--- app/src/main/java/com/example/cardviewmenu/HomeSlider.java
+++ app/src/main/java/com/example/cardviewmenu/HomeSlider.java
@@ -1,8 +1,15 @@
 package com.example.cardviewmenu;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.List;
 
@@ -21,6 +28,40 @@ public class HomeSlider extends FragmentStatePagerAdapter {
     @Override
     public int getCount(){
         return fragmentList.size();
+    }
+
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+
+        final Fragment o = fragmentList.get(position);
+        LayoutInflater inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_landing, container, false);
+
+        TextView title = (TextView) v.findViewById(R.id.txtWelcome);
+        title.setText(o.name);
+
+        ((ViewPager) container).addView(v);
+        return v;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
     }
 
 }
