ff75fd4fcc4f58b71fc971647b13d783316a0ef9
diaz <diazrezab@gmail.com>
2020-01-28 22:19:26 +0700

    add stepper layout untuk transaksi pulsa (belum sesuai aslinya)

--- app/src/main/java/com/example/cardviewmenu/MpoActivity.java
+++ app/src/main/java/com/example/cardviewmenu/MpoActivity.java
@@ -1,15 +1,31 @@
 package com.example.cardviewmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cardviewmenu.pulsafragments.PulsaActivity;
+
 public class MpoActivity extends AppCompatActivity {
+
+    private Button mBtLaunchPulsaActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mpo);
 
+        mBtLaunchPulsaActivity = (Button) findViewById(R.id.buttonPulsa);
+
+        mBtLaunchPulsaActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MpoActivity.this, PulsaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
