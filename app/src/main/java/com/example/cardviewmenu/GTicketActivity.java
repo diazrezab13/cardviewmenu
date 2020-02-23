199eeaebc8a9c30dfdd666549a24120509d9a962
Pramasita Gustiarum <pramasita.gustiarum@pegadaian.co.id>
2020-02-23 18:23:55 +0700

    Kuda Poni Menu

--- app/src/main/java/com/example/cardviewmenu/GTicketActivity.java
+++ app/src/main/java/com/example/cardviewmenu/GTicketActivity.java
@@ -1,6 +1,8 @@
 package com.example.cardviewmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class GTicketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gticket);
+    }
+    public void onClickPlay (View v){
+        Intent newIntent = new Intent(GTicketActivity.this, KudaPoniMenuActivity.class);
+        finish();
+        startActivity(newIntent);
     }
 }
---
+++ app/src/main/java/com/example/cardviewmenu/KudaPoniMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardviewmenu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KudaPoniMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menukudaponi);
+    }
+}
