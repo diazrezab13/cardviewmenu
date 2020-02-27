fcfbc1d9138a08577cbc2697a5f0d27ef9843dab
Pramasita Gustiarum <pramasita.gustiarum@pegadaian.co.id>
2020-02-27 16:09:08 +0700

    tambahan screen term and condition

--- app/src/main/java/com/example/cardviewmenu/KudaPoniMenuActivity.java
+++ app/src/main/java/com/example/cardviewmenu/KudaPoniMenuActivity.java
@@ -1,19 +1,53 @@
 package com.example.cardviewmenu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cardviewmenu.leaderboardfragment.LeaderboardActivity;
 
 public class KudaPoniMenuActivity extends AppCompatActivity {
+    Button popupAgreement;
+    TextView popupClosed;
+    RelativeLayout agreementLayout;
+    PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menukudaponi);
+
+        popupAgreement = (Button) findViewById(R.id.buttonTerm);
+
+        agreementLayout = (RelativeLayout) findViewById(R.id.layoutKudaPoni);
+        popupAgreement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = (LayoutInflater) KudaPoniMenuActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View newView = layoutInflater.inflate(R.layout.popup_termncond, null);
+                popupClosed =  newView.findViewById(R.id.txtAgree);
+                popupWindow = new PopupWindow(newView, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                popupWindow.showAtLocation(agreementLayout, Gravity.CENTER, 0, 0);
+                popupClosed.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+            }
+        });
+
+
     }
 
     public void onClickBack2 (View v){
@@ -27,4 +61,5 @@ public class KudaPoniMenuActivity extends AppCompatActivity {
         finish();
         startActivity(newIntent);
     }
+
 }
