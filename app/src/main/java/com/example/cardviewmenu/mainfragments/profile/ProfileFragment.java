19c5a7d481c4169150d68f85ab4cb5d63c5df9f4
Pramasita Gustiarum <pramasita.gustiarum@pegadaian.co.id>
2020-03-02 22:16:40 +0700

    profile listview belom bisa muncul tapi sudah dibuat

--- app/src/main/java/com/example/cardviewmenu/mainfragments/profile/ProfileFragment.java
+++ app/src/main/java/com/example/cardviewmenu/mainfragments/profile/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.cardviewmenu.mainfragments.profile;
 
 import android.os.Bundle;
+import android.app.ListActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,21 +18,22 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cardviewmenu.R;
 
+
+
 public class ProfileFragment extends Fragment {
     private ProfileViewModel profileViewModel;
+    ListView listView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        listView=(ListView) getView().findViewById(R.id.listViewProfile);
+        String[] listProfileMenu = getResources().getStringArray(R.array.array_profile);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.fragment_profile,
+                R.id.txtListProfile, listProfileMenu);
+       listView.setAdapter(adapter);
         return root;
     }
     }
