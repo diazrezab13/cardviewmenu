2da464a2f2fb5894b6b61ee8550846cd1d62bf4a
diaz <diazrezab@gmail.com>
2020-03-10 10:42:45 +0700

    implement listview on click

--- app/src/main/java/com/example/cardviewmenu/pulsafragments/Step1Fragment.java
+++ app/src/main/java/com/example/cardviewmenu/pulsafragments/Step1Fragment.java
@@ -1,30 +1,36 @@
 package com.example.cardviewmenu.pulsafragments;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.cardviewmenu.R;
+import com.example.cardviewmenu.SharedPrefManager;
 
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Step1Fragment extends Fragment implements View.OnClickListener {
+public class Step1Fragment extends Fragment {
 
     private View view;
     private EditText editTextNamaLengkap;
-    private Spinner spinnerPulsa;
-    private Button buttonNext;
+    private String Nominal;
+    private ListView listView;
 
     public Step1Fragment() {
         // Required empty public constructor
@@ -41,48 +47,79 @@ public class Step1Fragment extends Fragment implements View.OnClickListener {
 
     private void loadComponent() {
         editTextNamaLengkap = (EditText) view.findViewById(R.id.edit_text_nama_lengkap);
-        spinnerPulsa = (Spinner) view.findViewById(R.id.pulsaSpinner);
-        buttonNext = (Button) view.findViewById(R.id.button_next_fragment_step_1);
-        buttonNext.setOnClickListener(this);
+
+        //        buttonNext.setOnClickListener(this);
 
         ArrayList<Nominal> contacts = new ArrayList<>();
 
         contacts.add(new Nominal("-", "-"));
-        contacts.add(new Nominal("Rp 20000-", "1"));
-        contacts.add(new Nominal("Rp 25000-", "2"));
-        contacts.add(new Nominal("Rp 50000-", "3"));
-        contacts.add(new Nominal("Rp 100000-", "4"));
-        contacts.add(new Nominal("Rp 200000-", "5"));
+        editTextNamaLengkap.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        ArrayAdapter<Nominal> adapter =
-                new ArrayAdapter<Nominal>(getActivity().getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, contacts);
-        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
-
-        spinnerPulsa.setAdapter(adapter);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view == buttonNext) {
-            String nomor = editTextNamaLengkap.getText().toString().trim();
-            String nominal = spinnerPulsa.getSelectedItem().toString();
-
-            boolean InputEmpty = checkInput(nomor, nominal);
-
-            if(!InputEmpty){
-                PulsaActivity.goToStepOrangTua();
-                Step2Fragment step2Fragment = new Step2Fragment();
-                Bundle bundle = new Bundle();
-                bundle.putString("nomor", nomor);
-                bundle.putString("nominal", nominal);
-                step2Fragment.setArguments(bundle);
-                getFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.slide_in_from_right, R.anim.slide_out_from_right, R.anim.slide_in_from_left, R.anim.slide_out_from_left)
-                        .replace(R.id.frame_layout, step2Fragment)
-                        .addToBackStack(null)
-                        .commit();
             }
-        }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                listView = (ListView) view.findViewById(R.id.listNominalPulsa);
+                String[] listArray = {};
+
+                if(start >= 3){
+                    listArray = getResources().getStringArray(R.array.array_pulsa);
+                    ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), R.layout.list_tes, listArray);
+                    listView.setAdapter(adapter2);
+
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            String nomor = editTextNamaLengkap.getText().toString().trim();
+                            Nominal = "";
+
+                            if (position == 0){
+                                Nominal = "Rp 20000-";
+                            }else if (position == 1) {
+                                Nominal = "Rp 25000-";
+                            }else if (position == 2){
+                                Nominal = "Rp 50000-";
+                            }else if (position == 3){
+                                Nominal = "Rp 100000-";
+                            }else if (position == 4){
+                                Nominal = "Rp 200000-";
+                            }
+
+                            boolean InputEmpty = checkInput(nomor, Nominal);
+
+                            if(!InputEmpty){
+                                PulsaActivity.goToStepOrangTua();
+                                Step2Fragment step2Fragment = new Step2Fragment();
+                                Bundle bundle = new Bundle();
+                                bundle.putString("nomor", nomor);
+                                bundle.putString("nominal", Nominal);
+                                step2Fragment.setArguments(bundle);
+                                getFragmentManager().beginTransaction()
+                                        .setCustomAnimations(R.anim.slide_in_from_right, R.anim.slide_out_from_right, R.anim.slide_in_from_left, R.anim.slide_out_from_left)
+                                        .replace(R.id.frame_layout, step2Fragment)
+                                        .addToBackStack(null)
+                                        .commit();
+                            }
+
+                        }
+                    });
+                }else {
+                    ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listArray);
+                    listView.setAdapter(adapter2);
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
     }
 
     private boolean checkInput(String nomor, String nominal){
@@ -92,7 +129,7 @@ public class Step1Fragment extends Fragment implements View.OnClickListener {
             return true;
         }
 
-        if (nominal.matches("-")) {
+        if (nominal.matches("")) {
             Toast.makeText(this.getActivity(), "You did not enter nominal", Toast.LENGTH_SHORT).show();
             return true;
         }
